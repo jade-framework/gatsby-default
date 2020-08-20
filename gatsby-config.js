364440f58f32fcec8b26d6be728aecbe1a975732
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-08-20 11:06:55 -0700

    update header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Jade Depo Site`,
+    title: `Jade Demo Site`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
